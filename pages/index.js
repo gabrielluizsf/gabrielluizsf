9cebec51cf7df67b170a1a20b49abe3e775aa4c3
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-02 14:59:14 -0300

    home page

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+function HomePage() {
+  const enterpriseUrl = ""
+
+  return (
+      <><Head>
+      <title>| Gabriel Luiz | Home</title>
+      <link rel="icon" href="/favicon.svg" />
+    </Head><div className='max-w-2xl'>
+        <div className='pb-4'>
+          <p className='text-neutral-900 dark:text-neutral-50 text-2xl md:text-3xl lg:text-5xl font-bold tracking-tight'>Bem Vindo(a) ao meu Portifólio</p>
+           <p className='text-slate-800 dark:text-neutral-300 md:text-lg lg:text-xl italic py-px'>
+                Gabriel Luiz | Backend Developer 
+              </p>
+        </div>
+        <img src='https://raw.githubusercontent.com/egonelbre/gophers/master/.thumb/animation/2bit-sprite/demo.gif'></img>
+      </div></>
+  )
+}
+
+export default HomePage;
