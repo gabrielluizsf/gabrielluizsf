8c7cb48f2274df8060264b6c06c17038c18b983a
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-02 17:19:02 -0300

    refact: rename to portfólio

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ function HomePage() {
       <link rel="icon" href="/favicon.ico" />
     </Head><div className='max-w-2xl'>
         <div className='pb-4'>
-          <p className='text-neutral-900 dark:text-neutral-50 text-2xl md:text-3xl lg:text-5xl font-bold tracking-tight'>Bem Vindo(a) ao meu Portifólio</p>
+          <p className='text-neutral-900 dark:text-neutral-50 text-2xl md:text-3xl lg:text-5xl font-bold tracking-tight'>Bem Vindo(a) ao meu Portfólio</p>
            <p className='text-slate-800 dark:text-neutral-300 md:text-lg lg:text-xl italic py-px'>
                 Gabriel Luiz | Backend Developer 
               </p>
