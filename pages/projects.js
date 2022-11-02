c6cb84e28830d5b7a35a39c86323f7a31b8f24b4
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-02 15:00:51 -0300

    Projects Page

---
+++ pages/projects.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const projects = [
+    {
+        header: 'Personal',
+        items: []
+    },
+    {
+        header: 'School',
+        items: []
+    } 
+]
+
+function ProjectsPage() {
+    return (
+        <>
+            <Head>
+                <title>| Gabriel Luiz | Projects</title>
+                <link rel="icon" href="/favicon.svg" />
+            </Head>
+            <div className='max-w-2xl'>
+                <p className='text-neutral-900 dark:text-neutral-50 text-2xl md:text-3xl lg:text-5xl font-bold tracking-tight'>Projects</p>
+   
+            </div>
+            <div>
+            <Link 
+            href="https://photography-links.vercel.app/" 
+            target={'_blank'}
+            className="text-neutral-700 dark:text-neutral-50 text-2xl md:text-2xl lg:text-2xl font-bold tracking-tight"
+            >Social Network ↗ </Link>
+
+            </div>
+        </>
+    )
+}
+
+export default ProjectsPage
