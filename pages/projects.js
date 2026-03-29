8c8da3425e25cc994ac7bb675baaa4071d6949a1
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2026-03-29 12:40:10 -0300

    adding project url

--- pages/projects.js
+++ pages/projects.js
@@ -8,27 +8,27 @@ function ProjectsPage() {
                 <title>Projects</title>
                 <Link rel="icon" href="/favicon.ico" />
             </Head>
-        
+
             <div className="flex flex-col">
-                <img src="https://github.com/egonelbre/gophers/raw/master/.thumb/animation/gopher-dance-long-3x.gif" className="h-48 w-48 object-center mb-4" alt="Dancing Gopher" />  
+                <img src="https://github.com/egonelbre/gophers/raw/master/.thumb/animation/gopher-dance-long-3x.gif" className="h-48 w-48 object-center mb-4" alt="Dancing Gopher" />
                 <p className=" text-lg md:text-xl lg:text-2xl italic text-gray-800 dark:text-gray-300 mb-8">
                     Welcome to My Projects Page
                 </p>
-             
-                    {/*<Link 
-                        href="https://photography-links.vercel.app/" 
-                        target="_blank"
-                        className="text-blue-500 hover:underline text-lg md:text-xl lg:text-2xl font-bold tracking-tight"
-                    >
-                        Social Network ↗
-                    </Link> */}
-                    <Link 
-                        href="https://github.com/gabrielluizsf?tab=repositories" 
-                        target="_blank"
-                        className="text-blue-500 hover:underline text-lg md:text-xl lg:text-2xl font-bold "
-                    >
-                        Repositories ↗
-                    </Link>
+
+                <Link
+                    href="https://gabrielluizsf.github.io/mclaren_configurator/"
+                    target="_blank"
+                    className="text-blue-500 hover:underline text-lg md:text-xl lg:text-2xl font-bold tracking-tight"
+                >
+                    McLaren Configurator ↗
+                </Link>
+                <Link
+                    href="https://github.com/gabrielluizsf?tab=repositories"
+                    target="_blank"
+                    className="text-blue-500 hover:underline text-lg md:text-xl lg:text-2xl font-bold "
+                >
+                    Repositories ↗
+                </Link>
             </div>
         </>
     )
